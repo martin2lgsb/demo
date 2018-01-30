e3d7702b61a5e08025efdc5fac88cf2653eb5d30
m2lgsb <martin2lgsb@gmail.com>
2018-01-31 00:16:33 +0800

    composer

--- demo_php/demo_laravel/config/app.php
+++ demo_php/demo_laravel/config/app.php
@@ -177,6 +177,9 @@ return [
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
 
+        // barryvdh/laravel-ide-helper
+        Barryvdh\LaravelIdeHelper\IdeHelperServiceProvider::class,
+
     ],
 
     /*
