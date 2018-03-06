2575efd949a9eb94e3128061044c58fac4978628
Morgan <martin2lgsb@gmail.com>
2018-03-06 15:15:16 +0800

    lock

--- demo_java/demo_00/src/runoob/Thread/UnLockTest.java
+++ demo_java/demo_00/src/runoob/Thread/UnLockTest.java
@@ -1,8 +1,74 @@
 package runoob.Thread;
 
+import java.util.Date;
+import java.util.concurrent.Semaphore;
+import java.util.concurrent.TimeUnit;
+
 /**
  * Created by m2lgsb on 06/03/2018
  */
 public class UnLockTest {
-    // TODO: 06/03/2018  
+    public static String obj1 = "obj1";
+    public static final Semaphore a1 = new Semaphore(1);
+    public static String obj2 = "obj2";
+    public static final Semaphore a2 = new Semaphore(1);
+
+    public static void main(String[] args) {
+        LockAa la = new LockAa();
+        new Thread(la).start();
+        LockBb lb = new LockBb();
+        new Thread(lb).start();
+    }
+}
+
+class LockAa implements Runnable {
+    public void run() {
+        try {
+            System.out.println(new Date().toString() + " LockA 开始执行");
+            while (true) {
+                if (UnLockTest.a1.tryAcquire(1, TimeUnit.SECONDS)) {
+                    System.out.println(new Date().toString() + " LockA 锁住 obj1");
+                    if (UnLockTest.a2.tryAcquire(1, TimeUnit.SECONDS)) {
+                        System.out.println(new Date().toString() + " LockA 锁住 obj2");
+                        Thread.sleep(60 * 1000);
+                    } else {
+                        System.out.println(new Date().toString() + " LockA 锁 obj2 失败");
+                    }
+                } else {
+                    System.out.println(new Date().toString() + " LockA 锁 obj1 失败");
+                }
+                UnLockTest.a1.release();
+                UnLockTest.a2.release();
+                Thread.sleep(1000);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
+
+class LockBb implements Runnable {
+    public void run() {
+        try {
+            System.out.println(new Date().toString() + " LockB 开始执行");
+            while (true) {
+                if (UnLockTest.a2.tryAcquire(1, TimeUnit.SECONDS)) {
+                    System.out.println(new Date().toString() + " LockB 锁住 obj2");
+                    if (UnLockTest.a1.tryAcquire(1, TimeUnit.SECONDS)) {
+                        System.out.println(new Date().toString() + " LockB 锁住 obj1");
+                        Thread.sleep(60 * 1000);
+                    } else {
+                        System.out.println(new Date().toString() + " LockB 锁 obj1 失败");
+                    }
+                } else {
+                    System.out.println(new Date().toString() + " LockB 锁 obj2 失败");
+                }
+                UnLockTest.a1.release();
+                UnLockTest.a2.release();
+                Thread.sleep(10 * 1000);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
 }
