67e24a6bd071b08776bfa8fb3ebe42f4d9394303
m2lgsb <martin2lgsb@gmail.com>
2018-09-19 15:09:29 +0800

    feat: scala test_01

---
+++ demo_scala/demo_init/build.sbt
@@ -0,0 +1,5 @@
+name := "demo_init"
+
+version := "0.1"
+
+scalaVersion := "2.11.12"
---
+++ demo_scala/demo_init/src/main/scala/test_01.scala
@@ -0,0 +1,8 @@
+/**
+  * Created by m2lgsb on 2018/9/19 15:05
+  */
+object test_01 {
+  def main(args: Array[String]): Unit = {
+    println("test_01")
+  }
+}
