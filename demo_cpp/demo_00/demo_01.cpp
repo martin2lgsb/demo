1e9c9f8ac0efc28e405d8e1cf191dd213afb7f5b
m2lgsb <martin2lgsb@gmail.com>
2018-10-18 17:56:58 +0800

    feat: prime

--- demo_cpp/demo_00/demo_01.cpp
+++ demo_cpp/demo_00/demo_01.cpp
@@ -42,9 +42,28 @@ int fibonacci_02() {
     return 0;
 }
 
+int prime() {
+    int n, i, m = 0, flag = 0;
+    cout << "Enter the Number to check Prime: ";
+    cin >> n;
+    m = n/2;
+    for (i = 2; i <= m; i++) {
+        if (n % i == 0) {
+            cout << "Number is not Prime."<< endl;
+            flag = 1;
+            break;
+        }
+    }
+
+    if (flag == 0)
+        cout << "Number is Prime."<< endl;
+    return 0;
+}
+
+
 int main() {
 //    fibonacci_01();
 //    fibonacci_02();
-
+    prime();
 
 }
