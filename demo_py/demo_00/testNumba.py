307b1a459d9fffdd6ed2465d685a1a6897683332
m2lgsb <martin2lgsb@gmail.com>
2018-10-31 14:50:52 +0800

    feat: test numba

---
+++ demo_py/demo_00/testNumba.py
@@ -0,0 +1,15 @@
+from numba import jit
+import time
+
+# @jit
+def foo(x,y):
+        tt = time.time()
+        s = 0
+        for i in range(x,y):
+                s += i
+        print('Time used: {} sec'.format(time.time()-tt))
+        return s
+
+if __name__ == '__main__':
+
+    print(foo(1,100000000))
